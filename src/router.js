609a77a79d1997518be5cbc1b5b7ae985744e740
John Moore <john.moore@snh.gov.uk>
2020-12-11 10:32:29 +0000

    Add routes to router
    add the routes for the first half of the application
    
    Issue: Scottish-Natural-Heritage/Licensing#483

--- src/router.js
+++ src/router.js
@@ -4,13 +4,50 @@ const router = express.Router();
 // Import all the controllers.
 import {Page} from './controllers/_base.js';
 import StartController from './controllers/start.js';
+import UsageController from './controllers/usage.js';
+import TrapRegistrationNumberController from './controllers/trap-registration-number.js';
+import PostcodeController from './controllers/postcode.js';
 
 // Configure all of the pages and routes.
 
 router.use(
   Page({
     path: 'start',
+    positiveForward: 'usage',
     controller: StartController
+  })
+);
+
+router.use(
+  Page({
+    path: 'usage',
+    back: 'start',
+    positiveForward: 'trap-registration-number',
+    controller: UsageController
+  })
+);
+
+router.use(
+  Page({
+    path: 'trap-registration-number',
+    back: 'usage',
+    positiveForward: 'postcode',
+    controller: TrapRegistrationNumberController
+  })
+);
+
+router.use(
+  Page({
+    path: 'postcode',
+    back: 'trap-registration-number',
+    positiveForward: 'verification-success',
+    controller: PostcodeController
+  })
+);
+
+router.use(
+  Page({
+    path: 'verification-success'
   })
 );
 
