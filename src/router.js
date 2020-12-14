aecda2c1c16941aa2ef2c0ff82e34e218ab0c562
John Moore <john.moore@snh.gov.uk>
2020-12-14 09:11:40 +0000

    Add routes to router
    add the routes for the second half of the application
    
    Issue: Scottish-Natural-Heritage/Licensing#483

--- src/router.js
+++ src/router.js
@@ -8,8 +8,14 @@ import UsageController from './controllers/usage.js';
 import TrapRegistrationNumberController from './controllers/trap-registration-number.js';
 import PostcodeController from './controllers/postcode.js';
 
+import LoginController from './controllers/login.js';
+import TargetSpeciesController from './controllers/target-species.js';
+import DetailsListController from './controllers/details-list.js';
+import DetailsAddController from './controllers/details-add.js';
+import DetailsEditController from './controllers/details-edit.js';
+import ConfirmController from './controllers/confirm.js';
 // Configure all of the pages and routes.
-
+// First half of application/
 router.use(
   Page({
     path: 'start',
@@ -23,9 +29,12 @@ router.use(
     path: 'usage',
     back: 'start',
     positiveForward: 'trap-registration-number',
+    // TODO: Add no usage path as negative here.
     controller: UsageController
   })
 );
+
+// TODO: Add no usage path.
 
 router.use(
   Page({
@@ -48,6 +57,72 @@ router.use(
 router.use(
   Page({
     path: 'verification-success'
+  })
+);
+
+// Second half of application/
+router.use(
+  Page({
+    path: 'login',
+    positiveForward: 'target-species',
+    negativeForward: 'error',
+    controller: LoginController
+  })
+);
+
+router.use(
+  Page({
+    path: 'target-species',
+    back: 'login',
+    positiveForward: 'details-list',
+    // TODO: Add negative path here.
+    controller: TargetSpeciesController
+  })
+);
+
+// TODO: Add no target-species path.
+
+router.use(
+  Page({
+    path: 'details-list',
+    back: 'target-species',
+    positiveForward: 'confirm',
+    secondaryForward: 'details-add',
+    tertiaryForward: 'details-edit',
+    controller: DetailsListController
+  })
+);
+
+router.use(
+  Page({
+    path: 'details-add',
+    back: 'details-list',
+    positiveForward: 'details-list',
+    controller: DetailsAddController
+  })
+);
+
+router.use(
+  Page({
+    path: 'details-edit',
+    back: 'details-list',
+    positiveForward: 'details-list',
+    controller: DetailsEditController
+  })
+);
+
+router.use(
+  Page({
+    path: 'confirm',
+    back: 'details-list',
+    positiveForward: 'success',
+    controller: ConfirmController
+  })
+);
+
+router.use(
+  Page({
+    path: 'success'
   })
 );
 
