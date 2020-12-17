adfa8be7034ab908d5dfc560e8812c125893455d
murrayholmes96 <murray.holmes@nature.scot>
2020-12-17 13:17:17 +0000

    Altered no-usage content
    Issue: Scottish-Natural-Heritage/Licensing#480

--- cypress/integration/003b-no-usage.js
+++ cypress/integration/003b-no-usage.js
@@ -12,12 +12,14 @@ describe('no usage page ', function () {
     // POST `/start`
     cy.get('#main-content form button.naturescot-forward-button').click();
     // ~GET `/usage`~
+    // Click no
     cy.get('#main-content form input[type="radio"][value="no"]').click();
+    // POST `/usage`
     cy.get('#main-content form button.naturescot-forward-button').click();
   });
 
   it('should allow access if the user visits all the pages in order', function () {
     cy.visit('/no-usage');
-    cy.get('h1').should('contain', 'Trap Registration Return');
+    cy.get('h1').should('contain', 'Trap Return complete');
   });
 });
