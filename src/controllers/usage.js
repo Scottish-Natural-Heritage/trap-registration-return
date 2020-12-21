87defe7d08efcabc83d57433b62e60c77e1ad06a
murrayholmes96 <murray.holmes@nature.scot>
2020-12-21 12:25:09 +0000

    updated with comments
    comments are bad will need redone as i dont fully get it

--- src/controllers/usage.js
+++ src/controllers/usage.js
@@ -3,6 +3,8 @@ import {ReturnState} from './_base.js';
 const usageController = (request) => {
   // The usage page is where the user will state whether or not they have used
   // meat bait traps.
+
+  // Checks if user selected yes or no and sets return state approprietely
   if (request.body.usage === 'yes') {
     request.session.usageError = false;
     request.session.usage = true;
@@ -18,6 +20,7 @@ const usageController = (request) => {
   request.session.usageError = true;
   // The only way out of the usage page for now is onwards, so return success and continue
   // the form.
+
   request.session.usage = undefined;
   return ReturnState.Error;
 };
