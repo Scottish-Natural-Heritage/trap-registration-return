f4b384462476bbcecd9baa6778ffb98cbe436978
John Moore <john.moore@snh.gov.uk>
2021-01-20 15:23:54 +0000

    Add util with species array
    Issue: Scottish-Natural-Heritage/Licensing#418

---
+++ src/controllers/_util.js
@@ -0,0 +1,335 @@
+// An array of objects that is used to populate the options for the list of non target species.
+const nonTargetSpecies = () => {
+  return [
+    {
+      value: 'Recurvirostra avosetta',
+      text: 'Recurvirostra avosetta / Avocet'
+    },
+    {
+      value: 'Tyto alba',
+      text: 'Tyto alba / Barn owl'
+    },
+    {
+      value: 'Panurus biarmicus',
+      text: 'Panurus biarmicus / Bearded tit'
+    },
+    {
+      value: 'Merops apiaster',
+      text: 'Merops apiaster / Bee-eater'
+    },
+    {
+      value: 'Cygnus columbianus',
+      text: 'Cygnus columbianus / Bewick’s swan'
+    },
+    {
+      value: 'Botaurus stellaris',
+      text: 'Botaurus stellaris / Bittern'
+    },
+    {
+      value: 'Phoenicurus ochruros',
+      text: 'Phoenicurus ochruros / Black redstart'
+    },
+    {
+      value: 'Podiceps nigricollis',
+      text: 'Podiceps nigricollis / Black-necked grebe'
+    },
+    {
+      value: 'Limosa limosa',
+      text: 'Limosa limosa / Black-tailed godwit'
+    },
+    {
+      value: 'Gavia arctica',
+      text: 'Gavia arctica / Black-throated diver'
+    },
+    {
+      value: 'Luscinia svecica',
+      text: 'Luscinia svecica / Bluethroat'
+    },
+    {
+      value: 'Fringilla montifringilla',
+      text: 'Fringilla montifringilla / Brambling'
+    },
+    {
+      value: 'Buteo buteo',
+      text: 'Buteo buteo / Buzzard'
+    },
+    {
+      value: 'Tetrao urogallus',
+      text: 'Tetrao urogallus / Capercaillie'
+    },
+    {
+      value: 'Pyrrhocorax pyrrhocorax',
+      text: 'Pyrrhocorax pyrrhocorax / Chough'
+    },
+    {
+      value: 'Melanitta nigra',
+      text: 'Melanitta nigra / Common scoter'
+    },
+    {
+      value: 'Crex crex',
+      text: 'Crex crex / Corncrake'
+    },
+    {
+      value: 'Parus cristatus',
+      text: 'Parus cristatus / Crested tit'
+    },
+    {
+      value: 'Loxia curvirostra',
+      text: 'Loxia curvirostra / Crossbill'
+    },
+    {
+      value: 'Sylvia undata',
+      text: 'Sylvia undata / Dartford warbler'
+    },
+    {
+      value: 'Charadrius morinellus',
+      text: 'Charadrius morinellus / Dotterel'
+    },
+    {
+      value: 'Turdus pilaris',
+      text: 'Turdus pilaris / Fieldfare'
+    },
+    {
+      value: 'Regulus ignicapillus',
+      text: 'Regulus ignicapillus / Firecrest'
+    },
+    {
+      value: 'Anas querquedula',
+      text: 'Anas querquedula / Garganey'
+    },
+    {
+      value: 'Aquila chrysaetos',
+      text: 'Aquila chrysaetos / Golden eagle'
+    },
+    {
+      value: 'Oriolus oriolus',
+      text: 'Oriolus oriolus / Golden oriole'
+    },
+    {
+      value: 'Bucephala clangula',
+      text: 'Bucephala clangula / Goldeneye'
+    },
+    {
+      value: 'Accipter gentilis',
+      text: 'Accipter gentilis / Goshawk'
+    },
+    {
+      value: 'Gavia immer',
+      text: 'Gavia immer / Great Northern diver'
+    },
+    {
+      value: 'Tringa ochropus',
+      text: 'Tringa ochropus / Green sandpiper'
+    },
+    {
+      value: 'Tringa nebularia',
+      text: 'Tringa nebularia / Greenshank'
+    },
+    {
+      value: 'Anser anser',
+      text: 'Anser anser / Greylag goose'
+    },
+    {
+      value: 'Falco rusticolus',
+      text: 'Falco rusticolus / Gyr falcon'
+    },
+    {
+      value: 'Circus cyaneus',
+      text: 'Circus cyaneus / Hen harrier'
+    },
+    {
+      value: 'Falco subbuteo',
+      text: 'Falco subbuteo / Hobby'
+    },
+    {
+      value: 'Pernis apivorus',
+      text: 'Pernis apivorus / Honey buzzard'
+    },
+    {
+      value: 'Upupa epops',
+      text: 'Upupa epops / Hoopoe'
+    },
+    {
+      value: 'Falco tinnunculus',
+      text: 'Falco tinnunculus / Kestrel'
+    },
+    {
+      value: 'Alcedo atthis',
+      text: 'Alcedo atthis / Kingfisher'
+    },
+    {
+      value: 'Calcarius lapponicus',
+      text: 'Calcarius lapponicus / Lapland bunting'
+    },
+    {
+      value: 'Oceanodroma leucorhoa',
+      text: 'Oceanodroma leucorhoa / Leach’s petrel'
+    },
+    {
+      value: 'Larus minutus',
+      text: 'Larus minutus / Little gull'
+    },
+    {
+      value: 'Charadrius dubius',
+      text: 'Charadrius dubius / Little ringed plover'
+    },
+    {
+      value: 'Sterna albifrons',
+      text: 'Sterna albifrons / Little tern'
+    },
+    {
+      value: 'Clangula hyemalis',
+      text: 'Clangula hyemalis / Long-tailed duck'
+    },
+    {
+      value: 'Circus aeruginosus',
+      text: 'Circus aeruginosus / Marsh harrier'
+    },
+    {
+      value: 'Larus melanocephalus',
+      text: 'Larus melanocephalus / Mediterranean gull'
+    },
+    {
+      value: 'Falco columbarius',
+      text: 'Falco columbarius / Merlin'
+    },
+    {
+      value: 'Circus pygargus',
+      text: 'Circus pygargus / Montagu’s harrier'
+    },
+    {
+      value: 'Pandion haliaetus',
+      text: 'Pandion haliaetus / Osprey'
+    },
+    {
+      value: 'Loxia pytyopsittacus',
+      text: 'Loxia pytyopsittacus / Parrot Crossbill'
+    },
+    {
+      value: 'Falco perigrinus',
+      text: 'Falco perigrinus / Peregrine falcon'
+    },
+    {
+      value: 'Anas acuta',
+      text: 'Anas acuta / Pintail'
+    },
+    {
+      value: 'Ardea purpurea',
+      text: 'Ardea purpurea / Purple heron'
+    },
+    {
+      value: 'Calidris maritima',
+      text: 'Calidris maritima / Purple sandpiper'
+    },
+    {
+      value: 'Coturnix coturnix',
+      text: 'Coturnix coturnix / Quail'
+    },
+    {
+      value: 'Milvus milvus',
+      text: 'Milvus milvus / Red kite'
+    },
+    {
+      value: 'Lanius collurio',
+      text: 'Lanius collurio / Red-backed shrike'
+    },
+    {
+      value: 'Phalaropus lobatus',
+      text: 'Phalaropus lobatus / Red-necked phalarope'
+    },
+    {
+      value: 'Gavia stellata',
+      text: 'Gavia stellata / Red-throated diver'
+    },
+    {
+      value: 'Turdus iliacus',
+      text: 'Turdus iliacus / Redwing'
+    },
+    {
+      value: 'Sterna dougallii',
+      text: 'Sterna dougallii / Roseate tern'
+    },
+    {
+      value: 'Carpodacus erythrinus',
+      text: 'Carpodacus erythrinus / Rosefinch'
+    },
+    {
+      value: 'Philomachus pugnax',
+      text: 'Philomachus pugnax / Ruff'
+    },
+    {
+      value: 'Aythya marila',
+      text: 'Aythya marila / Scaup'
+    },
+    {
+      value: 'Loxia scotica',
+      text: 'Loxia scotica / Scottish Crossbill'
+    },
+    {
+      value: 'Serinus serinus',
+      text: 'Serinus serinus / Serin'
+    },
+    {
+      value: 'Eremophila alpestris',
+      text: 'Eremophila alpestris / Shore lark'
+    },
+    {
+      value: 'Podiceps auritus',
+      text: 'Podiceps auritus / Slavonian grebe'
+    },
+    {
+      value: 'Plectrophenax nivalis',
+      text: 'Plectrophenax nivalis / Snow bunting'
+    },
+    {
+      value: 'Nyctea scandiaca',
+      text: 'Nyctea scandiaca / Snowy owl'
+    },
+    {
+      value: 'Accipiter nisus',
+      text: 'Accipiter nisus / Sparrowhawk'
+    },
+    {
+      value: 'Platalea leucorodia',
+      text: 'Platalea leucorodia / Spoonbill'
+    },
+    {
+      value: 'Porzana porzana',
+      text: 'Porzana porzana / Spotted crake'
+    },
+    {
+      value: 'Calidris temminckii',
+      text: 'Calidris temminckii / Temminck’s stint'
+    },
+    {
+      value: 'Melanitta fusca',
+      text: 'Melanitta fusca / Velvet scoter'
+    },
+    {
+      value: 'Numenius phaeopus',
+      text: 'Numenius phaeopus / Whimbrel'
+    },
+    {
+      value: 'Gavia adamsii',
+      text: 'Gavia adamsii / White-billed diver'
+    },
+    {
+      value: 'Haliaeetus albicilla',
+      text: 'Haliaeetus albicilla / White-tailed eagle'
+    },
+    {
+      value: 'Cygnus cygnus',
+      text: 'Cygnus cygnus / Whooper swan'
+    },
+    {
+      value: 'Tringa glareola',
+      text: 'Tringa glareola / Wood sandpiper'
+    },
+    {
+      value: 'Jynx torquilla',
+      text: 'Jynx torquilla / Wryneck'
+    }
+  ];
+};
+
+export {nonTargetSpecies};
