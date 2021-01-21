3cc54c57f36ef6d9d8c745e8549bde7d609d5bb2
murrayholmes96 <murray.holmes@nature.scot>
2021-01-21 14:26:56 +0000

    added tests for trap-registration-number page input validation and error handling

--- cypress/integration/004-trap-registration-number.js
+++ cypress/integration/004-trap-registration-number.js
@@ -21,12 +21,60 @@ describe('Trap Registration Number page ', function () {
 
   it('should allow access if the user visits all the pages in order', function () {
     cy.visit('/trap-registration-number');
-    cy.get('h1').should('contain', 'Trap Registration Number');
+    cy.get('h1').should('contain', 'What is your trap registration number?');
   });
 
-  it('main button should navigate to postcode', function () {
+
+  it('Valid input followed by continue button should navigate to postcode', function () {
     cy.visit('/trap-registration-number');
+    cy.get('input').type('12345')
     cy.get('#main-content form button.naturescot-forward-button').click();
     cy.url().should('include', '/postcode');
+  });
+
+  it('Valid input followed by continue button should navigate to postcode', function () {
+    cy.visit('/trap-registration-number');
+    cy.get('input').type('     12345     ')
+    cy.get('#main-content form button.naturescot-forward-button').click();
+    cy.url().should('include', '/postcode');
+  });
+
+  it('Empty input followed by continue button should present an error and ask user to re-enter', function () {
+    cy.visit('/trap-registration-number');
+    cy.get('#main-content form button.naturescot-forward-button').click();
+    cy.url().should('include', '/trap-registration-number');
+
+    cy.get('h2#error-summary-title').should('contain', 'There is a problem');
+    cy.get('#registration-number-error').should('contain', 'Enter your trap registration number');
+  });
+
+  it('Whitespace input followed by continue button should present an error and ask user to re-enter', function () {
+    cy.visit('/trap-registration-number');
+    cy.get('input').type('     ')
+    cy.get('#main-content form button.naturescot-forward-button').click();
+    cy.url().should('include', '/trap-registration-number');
+
+    cy.get('h2#error-summary-title').should('contain', 'There is a problem');
+    cy.get('#registration-number-error').should('contain', 'Enter your trap registration number');
+  });
+
+  it('Invalid input followed by continue button should present an error and ask user to re-enter', function () {
+    cy.visit('/trap-registration-number');
+    cy.get('input').type('123456')
+    cy.get('#main-content form button.naturescot-forward-button').click();
+    cy.url().should('include', '/trap-registration-number');
+
+    cy.get('h2#error-summary-title').should('contain', 'There is a problem');
+    cy.get('#registration-number-error').should('contain', 'Enter your trap registration number');
+  });
+
+  it('Invalid input followed by continue button should present an error and ask user to re-enter', function () {
+    cy.visit('/trap-registration-number');
+    cy.get('input').type('abcde')
+    cy.get('#main-content form button.naturescot-forward-button').click();
+    cy.url().should('include', '/trap-registration-number');
+
+    cy.get('h2#error-summary-title').should('contain', 'There is a problem');
+    cy.get('#registration-number-error').should('contain', 'Enter your trap registration number');
   });
 });
