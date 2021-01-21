bac75a0cd9f094bd23735c723a3ff18938fa4327
murrayholmes96 <murray.holmes@nature.scot>
2021-01-21 15:25:57 +0000

    issue #479 fixed linting errors and simplified regular expression

--- src/controllers/trap-registration-number.js
+++ src/controllers/trap-registration-number.js
@@ -1,33 +1,35 @@
 import {ReturnState} from './_base.js';
 
-//Cleans input checking if its undefined and trimming white space from the text
+// Cleans input checking if its undefined and trimming white space from the text
 const cleanInput = (body) => {
   return {
     registrationNumber: body.registrationNumber === undefined ? undefined : body.registrationNumber.trim()
-  }
-}
+  };
+};
+
 const trapRegistrationNumberController = (request) => {
   // The trap registration number page is where the user will enter their trap registration
   // number.
   const cleanForm = cleanInput(request.body);
   request.session.registrationNumber = cleanForm.registrationNumber;
 
-  //Clear error state
+  // Clear error state
   request.session.registrationNumberError = false;
-  //Check if registration number is valid and isn't empty
+  // Check if registration number is valid and isn't empty
   if (
     request.session.registrationNumber === undefined ||
     request.session.registrationNumber.trim() === '' ||
-    ! new RegExp('^[0-9]{1,5}$').test(request.session.registrationNumber)
+    !/^\d{1,5}$/.test(request.session.registrationNumber)
   ) {
     request.session.registrationNumberError = true;
   }
 
-  //Set error state
+  // Set error state
   if (request.session.registrationNumberError) {
     return ReturnState.Error;
   }
-  //Return positive state for progression
+  // Return positive state for progression
+
   return ReturnState.Positive;
 };
 
