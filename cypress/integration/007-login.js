6ea2638530b1e9893360074192e006787dad82b8
Mike Coats <mike.coats@nature.scot>
2021-01-29 16:41:29 +0000

    Update the login test-link to include a token
    This uses the 100 year long token from the mock axios module.

--- cypress/integration/007-login.js
+++ cypress/integration/007-login.js
@@ -1,6 +1,11 @@
 describe('Login page', function () {
-  it('main button should navigate to usage', function () {
-    cy.visit('/login');
+  it('with a valid token then main button should navigate to usage', function () {
+    cy.visit(
+      '/login?token=' +
+        'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.' +
+        'eyJleHAiOjQ3Njc2NzQ1NTgsInN1YiI6Ii0xIn0.' +
+        'XSHX6QB8robVaEuXVeHKbBed13uAdWvLBaNeGCYPAWWlw7Fm7bafXMPUQQE69TNc8DbjUgaRDxKvS2ju5uZziw'
+    );
     cy.get('#main-content form button.naturescot-forward-button').click();
     cy.url().should('include', '/target-species');
   });
