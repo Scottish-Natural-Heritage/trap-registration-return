0e8ff5e6da39cda7655a052fcd5ad623fdc4512f
Mike Coats <mike.coats@nature.scot>
2021-02-11 13:44:21 +0000

    Fix a line-length linting error
    Issue: none

--- src/server.js
+++ src/server.js
@@ -11,6 +11,8 @@ import app from './app.js';
 app.listen(config.port, () => {
   console.log(`Server listening on http://localhost:${config.port}${config.pathPrefix}.`);
   if (process.env.TRR_TEST) {
-    console.log(`Log in with http://localhost:${config.port}${config.pathPrefix}/login?token=${counterpart100yearToken}`);
+    console.log(
+      `Log in with http://localhost:${config.port}${config.pathPrefix}/login?token=${counterpart100yearToken}`
+    );
   }
 });
