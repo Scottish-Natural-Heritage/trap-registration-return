8634908bbcfeb34ce4c91e86c4a15a85b08578bb
Mike Coats <mike.coats@nature.scot>
2021-02-11 13:31:37 +0000

    Add a nicety for the developers
    When launched in test mode it now prints to console the link to login to
    get to the second half of the app.
    
    Issue: none

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 
 // Load the config.
 import config from './config/app.js';
+import {counterpart100yearToken} from './config/http-request.js';
 
 // Load the app.
 import app from './app.js';
@@ -9,4 +10,7 @@ import app from './app.js';
 // Run it.
 app.listen(config.port, () => {
   console.log(`Server listening on http://localhost:${config.port}${config.pathPrefix}.`);
+  if (process.env.TRR_TEST) {
+    console.log(`Log in with http://localhost:${config.port}${config.pathPrefix}/login?token=${counterpart100yearToken}`);
+  }
 });
