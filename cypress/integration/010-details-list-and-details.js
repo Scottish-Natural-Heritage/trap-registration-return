13778e8bc1e57d2e721e44ca2c76e8641135065e
Paul Cox <paul.cox@nature.scot>
2021-07-08 11:30:25 +0100

    Update integration tests. Issue Licensing 743

--- cypress/integration/010-details-list-and-details.js
+++ cypress/integration/010-details-list-and-details.js
@@ -1,11 +1,11 @@
-describe('details list page directly', () => {
+describe('details page directly', () => {
   it('should prevent access', () => {
-    cy.visit('/details-list', {failOnStatusCode: false});
+    cy.visit('/details', {failOnStatusCode: false});
     cy.get('h1').should('contain', 'there is a problem with the service');
   });
 });
 
-describe('details list page ', () => {
+describe('details page ', () => {
   beforeEach(() => {
     // GET `/login`
     cy.visit(
@@ -21,32 +21,42 @@ describe('details list page ', () => {
     cy.get('#main-content form input[type="radio"][value="yes"]').click();
     // POST `/target-species`
     cy.get('#main-content form button.naturescot-forward-button').click();
-    // ~GET `/details-list`~
+    // ~GET `/details`~
   });
 
   it('should allow access if the user visits all the pages in order', () => {
-    cy.visit('/details-list');
-    cy.get('h1').should('contain', 'Details of non-target species');
+    cy.visit('/details');
+    cy.get('h1').should('contain', 'What are the details of the non-target species you caught?');
   });
 
-  it('add button should navigate to details-add page', () => {
-    cy.visit('/details-list');
-    cy.get('#main-content form button.naturescot-button--add').click();
-    cy.url().should('include', '/details');
+  it('completed details form and clicking forward should navigate to details list page', () => {
+    cy.visit('/details');
+
+    cy.get('input[type="text"]#current-grid-reference').type('NO 08529 29128', {delay: 1});
+    cy.get('#main-content form input[type="radio"][value="otherSpecies"]').click();
+    cy.get('input[type="text"]#current-other-species-caught').type('Test Species', {delay: 1});
+    cy.get('input[type="text"]#current-number-caught').type('3', {delay: 1});
+    cy.get('#main-content form input[type="radio"][value="Larsen pod"]').click();
+    cy.get('textarea#current-comment').type('Test comment', {delay: 1});
+
+    cy.get('#main-content form button.naturescot-forward-button').click();
+
+    cy.url().should('include', '/details-list');
   });
 
-  it('continue button should navigate to same page if nothing is added with errors', () => {
-    cy.visit('/details-list');
+  it('add button on list page should navigate to add page and submit empty form and see errors', () => {
+    cy.visit('/details');
+
+    cy.get('input[type="text"]#current-grid-reference').type('NO 08529 29128', {delay: 1});
+    cy.get('#main-content form input[type="radio"][value="otherSpecies"]').click();
+    cy.get('input[type="text"]#current-other-species-caught').type('Test Species', {delay: 1});
+    cy.get('input[type="text"]#current-number-caught').type('3', {delay: 1});
+    cy.get('#main-content form input[type="radio"][value="Larsen pod"]').click();
+    cy.get('textarea#current-comment').type('Test comment', {delay: 1});
+
     cy.get('#main-content form button.naturescot-forward-button').click();
     cy.url().should('include', '/details-list');
 
-    cy.get('h2#error-summary-title').should('contain', 'There is a problem');
-    cy.get('.govuk-error-summary ul li a').should('contain', 'Add at least one non-target species');
-    cy.get('form .govuk-form-group--error').should('contain', 'Add at least one non-target species');
-  });
-
-  it('add button should navigate to add page enter empty form and see errors', () => {
-    cy.visit('/details-list');
     cy.get('#main-content form button.naturescot-button--add').click();
     cy.url().should('include', '/details');
 
@@ -60,8 +70,19 @@ describe('details list page ', () => {
       .and('contain', 'Select the trap type that was used');
   });
 
-  it('add button should navigate to add page enter a semi empty form (schedule1Birds) and see errors', () => {
-    cy.visit('/details-list');
+  it('add button on list page should navigate to add page and submit semi-empty form (schedule1Birds) and see errors', () => {
+    cy.visit('/details');
+
+    cy.get('input[type="text"]#current-grid-reference').type('NO 08529 29128', {delay: 1});
+    cy.get('#main-content form input[type="radio"][value="otherSpecies"]').click();
+    cy.get('input[type="text"]#current-other-species-caught').type('Test Species', {delay: 1});
+    cy.get('input[type="text"]#current-number-caught').type('3', {delay: 1});
+    cy.get('#main-content form input[type="radio"][value="Larsen pod"]').click();
+    cy.get('textarea#current-comment').type('Test comment', {delay: 1});
+
+    cy.get('#main-content form button.naturescot-forward-button').click();
+    cy.url().should('include', '/details-list');
+
     cy.get('#main-content form button.naturescot-button--add').click();
     cy.url().should('include', '/details');
 
@@ -77,8 +98,21 @@ describe('details list page ', () => {
     cy.get('.govuk-error-summary ul li a').should('contain', 'Enter the non-target species caught');
   });
 
-  it('add button should navigate to add page enter a semi empty form (other species) and see errors', () => {
-    cy.visit('/details-list');
+
+
+  it('add button on list page should navigate to add page and submit semi-empty form (other species) and see errors', () => {
+    cy.visit('/details');
+
+    cy.get('input[type="text"]#current-grid-reference').type('NO 08529 29128', {delay: 1});
+    cy.get('#main-content form input[type="radio"][value="otherSpecies"]').click();
+    cy.get('input[type="text"]#current-other-species-caught').type('Test Species', {delay: 1});
+    cy.get('input[type="text"]#current-number-caught').type('3', {delay: 1});
+    cy.get('#main-content form input[type="radio"][value="Larsen pod"]').click();
+    cy.get('textarea#current-comment').type('Test comment', {delay: 1});
+
+    cy.get('#main-content form button.naturescot-forward-button').click();
+    cy.url().should('include', '/details-list');
+
     cy.get('#main-content form button.naturescot-button--add').click();
     cy.url().should('include', '/details');
 
@@ -94,26 +128,19 @@ describe('details list page ', () => {
     cy.get('.govuk-error-summary ul li a').should('contain', 'Enter the name of the non-target species caught');
   });
 
-  it('add button should navigate to add page enter a semi empty form (other species) and see errors', () => {
-    cy.visit('/details-list');
-    cy.get('#main-content form button.naturescot-button--add').click();
-    cy.url().should('include', '/details');
+  it('main button on details-list page should navigate to confirm page', () => {
+    cy.visit('/details');
 
     cy.get('input[type="text"]#current-grid-reference').type('NO 08529 29128', {delay: 1});
     cy.get('#main-content form input[type="radio"][value="otherSpecies"]').click();
     cy.get('input[type="text"]#current-other-species-caught').type('Test Species', {delay: 1});
-    cy.get('input[type="text"]#current-number-caught').type('0', {delay: 1});
+    cy.get('input[type="text"]#current-number-caught').type('3', {delay: 1});
     cy.get('#main-content form input[type="radio"][value="Larsen pod"]').click();
     cy.get('textarea#current-comment').type('Test comment', {delay: 1});
 
     cy.get('#main-content form button.naturescot-forward-button').click();
-    cy.url().should('include', '/details');
+    cy.url().should('include', '/details-list');
 
-    cy.get('.govuk-error-summary ul li a').should('contain', 'Enter the number of non-target species caught');
-  });
-
-  it('main button should navigate to confirm page', () => {
-    cy.visit('/details-list');
     cy.get('#main-content form button.naturescot-button--add').click();
     cy.url().should('include', '/details');
 
