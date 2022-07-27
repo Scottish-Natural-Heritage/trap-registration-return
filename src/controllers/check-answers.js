78663ecdf0332e62fe9e9f8109d6de30a8aedeb4
Paul Cox <paul.cox@nature.scot>
2022-07-27 11:42:48 +0100

    Remove some unneeded code. Issue Licensing 1371

--- src/controllers/check-answers.js
+++ src/controllers/check-answers.js
@@ -1,11 +1,6 @@
 import {ReturnState} from './_base.js';
 
-const checkAnswersController = (request) => {
-  // The check your answers page is where our cookie banner is placed, so by progressing past
-  // this page, we know the user's seen the banner, so we don't need to show it
-  // again.
-  request.session.seenCookie = true;
-
+const checkAnswersController = (_request) => {
   // The only way out of the check your answers page is onwards, so return success and begin
   // the form.
   return ReturnState.Positive;
--- src/controllers/year.js
+++ src/controllers/year.js
@@ -1,11 +1,6 @@
 import {ReturnState} from './_base.js';
 
-const yearController = (request) => {
-  // The year page is where our cookie banner is placed, so by progressing past
-  // this page, we know the user's seen the banner, so we don't need to show it
-  // again.
-  request.session.seenCookie = true;
-
+const yearController = (_request) => {
   // The only way out of the year page is onwards, so return success and begin
   // the form.
   return ReturnState.Positive;
