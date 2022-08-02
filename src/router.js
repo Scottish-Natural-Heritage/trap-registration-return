a6b14747176f0a2f0e519a1c9155b3e36547e46c
murrayholmes96 <murray.holmes@nature.scot>
2022-08-02 14:50:57 +0100

    fix routes for positive and negative states
    Issue: Scottish-Natural-Heritage/Licensing#660

--- src/router.js
+++ src/router.js
@@ -79,6 +79,7 @@ router.use(
     path: 'meat-baits-in-traps',
     back: 'year',
     positiveForward: 'how-many-traps-used',
+    negativeForward: 'check-answers',
     controller: MeatBaitsInTrapsController
   })
 );
