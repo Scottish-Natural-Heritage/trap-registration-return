c8bdb58d9cbabe3c94319cec6f2d94ee10c89f1e
Jen Foster <jennifer.massie@nature.scot>
2022-08-04 11:28:08 +0100

    added controller to check for a valid number and templete njk file with govukinputs and validation - Scottish-Natural-Heritage/Licensing#663

--- src/controllers/how-many-traps-used.js
+++ src/controllers/how-many-traps-used.js
@@ -1,8 +1,56 @@
 import {ReturnState} from './_base.js';
 
-const howManyTrapsUsedController = (_request) => {
-  // The only way out of the how many traps used page is onwards, so return success and begin
-  // the form.
+/**
+ * Check to see if the user supplied string looks like a number.
+ *
+ * @param {string} number A number.
+ * @returns {boolean} True if this looks like a valid number,
+ * otherwise false.
+ */
+ const validNumber = (number) => {
+  if (number === undefined) {
+    return false;
+  }
+
+  if (number.trim() === '') {
+    return false;
+  }
+
+  const testParse = Number.parseInt(number, 10);
+  if (Number.isNaN(testParse)) {
+    return false;
+  }
+
+  if (testParse <= 0) {
+    return false;
+  }
+
+  return true;
+};
+
+const howManyTrapsUsedController = (request) => {
+
+  request.session.noLarsenMateError = false;
+  request.session.noLarsenPodError = false;
+
+  // Did the user add an amount for larsen mate.
+  if (!validNumber(request.body.numberLarsenMateCaught)) {
+    // Check it's a valid number then save the number into session.
+    request.session.noLarsenMateError = true;
+  }
+
+  if (!validNumber(request.body.numberLarsenPodCaught)) {
+    // Check it's a valid number then save the number into session.
+    request.session.noLarsenPodError = true;
+  }
+
+  if (request.session.noLarsenMateError || request.session.noLarsenPodError) {
+    return ReturnState.Error;
+  }
+
+  request.session.numberLarsenMateCaught = request.body.numberLarsenMateCaught;
+  request.session.numberLarsenPodCaught = request.body.numberLarsenPodCaught;
+
   return ReturnState.Positive;
 };
 
