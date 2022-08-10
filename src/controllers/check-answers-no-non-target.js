a367e4e2c2f83da7eb1b1e1d93688f5c08c3e722
Paul Cox <paul.cox@nature.scot>
2022-08-10 08:25:48 +0100

    Update remaining 2 check-answers controllers with code to post return. Issue Licensing 661 1385

--- src/controllers/check-answers-no-non-target.js
+++ src/controllers/check-answers-no-non-target.js
@@ -1,7 +1,43 @@
+import axios from '../config/http-request.js';
+import config from '../config/app.js';
 import {ReturnState} from './_base.js';
 
-const checkAnswersNoNonTargetController = (_request) => {
-  // The only way out of the check-answers-no-non-target page is onwards, so return Positive.
+const checkAnswersNoNonTargetController = async (request) => {
+  // Declare errors and set to false.
+  request.session.missingConfirmValue = false;
+  request.session.apiError = false;
+
+  // Did the user click the confirm checkbox?
+  request.session.confirmedDeclaration = request.body.confirm === 'confirm';
+
+  // If the user didn't click the confirm checkbox this is an error.
+  request.session.missingConfirmValue = !request.session.confirmedDeclaration;
+
+  // If we have an error return the error state to let the user know immediately.
+  if (request.session.missingConfirmValue) {
+    return ReturnState.Error;
+  }
+
+  // If we made it here the user has confirmed their return so get the data ready to send.
+  const newReturn = {
+    noMeatBaitsUsed: request.session.noMeatBaitsUsed,
+    year: request.session.year,
+    numberLarsenMate: request.session.numberLarsenMateCaught,
+    numberLarsenPod: request.session.numberLarsenPodCaught,
+    nonTargetSpeciesToReport: request.session.targetSpecies,
+    nonTargetSpeciesCaught: request.session.detailsList
+  };
+
+  // And send the return data to the API.
+  try {
+    await axios.post(config.apiEndpoint + '/registrations/' + request.session.loggedInRegNo + '/return', newReturn);
+  } catch (error) {
+    console.log('Error creating new return:' + error);
+    request.session.apiError = true;
+    return ReturnState.Error;
+  }
+
+  // All went well so proceed to success page.
   return ReturnState.Positive;
 };
 
