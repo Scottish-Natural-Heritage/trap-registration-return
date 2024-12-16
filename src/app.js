6e9db56e1f5a09c57b043b811098473ce630772a
ArronChaveAC <125369855+ArronChaveAC@users.noreply.github.com>
2024-12-16 17:53:48 +0000

    rollback to main (#110)

--- src/app.js
+++ src/app.js
@@ -61,7 +61,7 @@ app.use(
       // We need to set the secure attribute to true as Caddy doesn't
       // currently rewrite the attribute for us in the way Nginx did.
       // If we're running tests don't set it as it'll break Cypress.
-      secure: process.env.TRR_TEST || process.env.TRR_DEV ? false : true
+      secure: process.env.TRR_TEST ? false : true
     },
     store: new MemoryStore({
       checkPeriod: sessionDuration
